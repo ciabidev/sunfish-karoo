2026b9b5220f560e3a665aa5d68f3cbc1626c386
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-11-26 21:45:09 -0500

    add followed luck party detection

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -19,5 +19,19 @@ module.exports = {
             console.error(`${message.author.username} survived the whirlpool and was not banned: `, err);
         }
     }
+
+    // check if ?luckping or luckping is sent in the luck channel
+
+    const LUCK_CHANNEL_ID = "1393401001029140610"; // replace with your actual channel ID
+    const LUCK_PING_ROLE_ID = "1393774571336896592"; // replace with your actual role ID
+    if (channel.id === LUCK_CHANNEL_ID) {
+        const messageContent = message.content.toLowerCase();
+        // make sure the message is from a followed server
+        const isFromFollowedServer = message.webhookId !== null;
+        if (!isFromFollowedServer) return;
+        if (messageContent.includes("?luckparty") || messageContent.includes("luckping")) {
+            await channel.send(`luck party from followed server: <@${message.author.id}> (<@&${LUCK_PING_ROLE_ID}>)`);
+        }
+    }
   },
 }
