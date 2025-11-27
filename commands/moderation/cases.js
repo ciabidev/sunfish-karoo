cbe865f09d3352cb2f3d38c4f08b78fa17a33023
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-11-26 20:09:31 -0500

    fix: pagination buttons on moderation cases command didnt work

--- commands/moderation/cases.js
+++ commands/moderation/cases.js
@@ -7,6 +7,7 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  ComponentType,
 } = require("discord.js");
 
 module.exports = {
@@ -75,61 +76,62 @@ module.exports = {
 
     const components = [mainText, pageSelector];
 
-    const message = await interaction.reply({
+    const response = await interaction.reply({
+      content: '',
       components,
       flags: MessageFlags.IsComponentsV2,
       withResponse: true,
     });
 
-    const collector = message.createMessageComponentCollector({
-      filter: (i) => i.isButton(),
-      time: 1000 * 60 * 15,
-    }); // create a collector to handle additional interaction events
+    const collector = response.resource.message.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 3_600_000, // 1 hour
+    });
+  
 
-    // handle button clicks
-    collector.on("collect", async (i) => {
-      if (i.customId === "previous-page") {
-        currentPage--;
-        if (currentPage < 0) {
-          currentPage = pages.length - 1;
-        }
-      } else if (i.customId === "next-page") {
-        currentPage++;
-        if (currentPage >= pages.length) {
-          currentPage = 0;
-        }
+  collector.on('collect', async (i) => {
+    if (i.customId === "previous-page") {
+      currentPage--;
+      if (currentPage < 0) {
+        currentPage = pages.length - 1;
       }
+    } else if (i.customId === "next-page") {
+      currentPage++;
+      if (currentPage >= pages.length) {
+        currentPage = 0;
+      }
+    }
 
-      const newMainText = new ContainerBuilder().addTextDisplayComponents(
-        (t) => t.setContent(`## Moderation Cases for <@${targetUser.id}>`),
-        (t) => t.setContent(`**Points:** ${points}\n`)
+    const newMainText = new ContainerBuilder().addTextDisplayComponents(
+      (t) => t.setContent(`## Moderation Cases for <@${targetUser.id}>`),
+      (t) => t.setContent(`**Points:** ${points}\n`)
+    );
+
+    for (const caseData of pages[currentPage]) {
+      const action = caseData.action;
+      const reason = caseData.reason;
+      const actionedById = caseData.actioned_by;
+      const durationMs = caseData.duration_ms;
+      const pointsDelta = caseData.points_delta;
+      const caseId = caseData.id;
+      const formattedDuration = interaction.client.modules.formatMilliseconds(durationMs);
+      const dateUnix = Math.floor(new Date(caseData.created_at).getTime() / 1000);
+
+      newMainText.addTextDisplayComponents(
+        (t) => t.setContent(`### ${action} | case #${caseId}`),
+        (t) => t.setContent(`Actioned by <@${actionedById}> <t:${dateUnix}:R>`),
+        (t) => t.setContent(`**Reason: ** ${reason ? reason : "No reason provided"}\n`),
+        ...(durationMs ? [(t) => t.setContent(`**Duration: ** ${formattedDuration}\n`)] : []),
+        (t) => t.setContent(`**Point Change: ** ${pointsDelta > 0 ? "+" : ""}${pointsDelta}\n`)
       );
 
-      for (const caseData of pages[currentPage]) {
-        const action = caseData.action;
-        const reason = caseData.reason;
-        const actionedById = caseData.actioned_by;
-        const durationMs = caseData.duration_ms;
-        const pointsDelta = caseData.points_delta;
-        const caseId = caseData.id;
-        const formattedDuration = interaction.client.modules.formatMilliseconds(durationMs);
-        const dateUnix = Math.floor(new Date(caseData.created_at).getTime() / 1000);
+      newMainText.addSeparatorComponents((separator) =>
+        separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large)
+      );
+    }
 
-        newMainText.addTextDisplayComponents(
-          (t) => t.setContent(`### ${action} | case #${caseId}`),
-          (t) => t.setContent(`Actioned by <@${actionedById}> <t:${dateUnix}:R>`),
-          (t) => t.setContent(`**Reason: ** ${reason ? reason : "No reason provided"}\n`),
-          ...(durationMs ? [(t) => t.setContent(`**Duration: ** ${formattedDuration}\n`)] : []),
-          (t) => t.setContent(`**Point Change: ** ${pointsDelta > 0 ? "+" : ""}${pointsDelta}\n`)
-        );
+    await i.update({ components: [newMainText, pageSelector] }); // update the message with the new components
+  });
 
-        newMainText.addSeparatorComponents((separator) =>
-          separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large)
-        );
-      }
-
-        await i.deferUpdate();
-        await i.message.edit({ components: [newMainText, pageSelector] }); // update the message with the new components
-    });
   },
 };
