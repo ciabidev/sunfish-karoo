7b7249ee92efb6a74b27dbdd3646836a10a6d256
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-12-05 22:43:37 -0500

    add epicenter ping detection

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,36 +3,59 @@ const { Events, MessageType } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
-    
     const isFromFollowedServer = message.webhookId !== null;
     console.log("isFromFollowedServer: ", isFromFollowedServer);
     const channel = message.channel;
     const WHIRLPOOL_CHANNEL_ID = "1435802233655656549"; // replace with your actual channel ID
     if (channel.id === WHIRLPOOL_CHANNEL_ID) {
-        await message.delete();
+      await message.delete();
 
-        try {
-            await message.guild.members.ban(message.author.id, {
-                reason: "Spam bot detected",
-                days: 1000000000,
-            });
-        } catch (err) {
-            console.error(`${message.author.username} survived the whirlpool and was not banned: `, err);
-        }
+      try {
+        await message.guild.members.ban(message.author.id, {
+          reason: "Spam bot detected",
+          days: 1000000000,
+        });
+      } catch (err) {
+        console.error(
+          `${message.author.username} survived the whirlpool and was not banned: `,
+          err
+        );
+      }
     }
+
+    /* -------------------
+    Followed Luck Channel
+    -------------------- */
 
     // check if ?luckping or luckping is sent in the luck channel
+    const serverPartnerText = `\n-# this host is part of our <#1393424277180776619>`;
+    if (isFromFollowedServer) {
 
-    const LUCK_CHANNEL_ID = "1393401001029140610"; // replace with your actual channel ID
-    const LUCK_PING_ROLE_ID = "1393774571336896592"; // replace with your actual role ID
-    if (channel.id === LUCK_CHANNEL_ID) {
+      const FOLLOWED_LUCK_CHANNEL_ID = "1393401001029140610"; // replace with your actual channel ID
+      const LUCK_PING_ROLE_ID = "1393774571336896592"; // replace with your actual role ID
+      if (channel.id === FOLLOWED_LUCK_CHANNEL_ID) {
         const messageContent = message.content.toLowerCase();
         // make sure the message is from a followed server
-        if (!isFromFollowedServer) return;
-        if (messageContent.includes("?luckparty") || messageContent.includes("luckping")) {
-            console.log("Luck ping message received: ", messageContent);
-            await channel.send(`<@${message.author.id}> is hosting a luck party! (<@&${LUCK_PING_ROLE_ID}>) \n-# this server is a partnered server. See all partnered servers: `);
+        if (messageContent.includes("?luckparty") || messageContent.includes("luckping") || messageContent.includes("@Luck")) {
+          console.log("Luck ping message received: ", messageContent);
+          await channel.send(
+            `<@${message.author.id}> is hosting a luck party!${serverPartnerText}`
+          );
         }
+      }
+
+      const FOLLOWED_EPICENTER_CHANNEL_ID = "1436513161283895416"; // replace with your actual channel ID
+      const EPICENTER_PING_ROLE_ID = "1436523924472070187"; // replace with your actual role ID
+      if (channel.id === FOLLOWED_EPICENTER_CHANNEL_ID) {
+        const messageContent = message.content.toLowerCase();
+        // make sure the message is from a followed server
+        if (messageContent.includes("epiping")) {
+          console.log("Epicenter ping message received: ", messageContent);
+          await channel.send(
+            `<@${message.author.id}> is hosting a trip to the epicenter! (<@&${EPICENTER_PING_ROLE_ID}${serverPartnerText}>) `
+          );
+        }
+      }
     }
   },
-}
+};
