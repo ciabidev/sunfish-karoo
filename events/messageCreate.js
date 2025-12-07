257672eed23662dd539a7762649283a37e704732
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-12-07 17:38:00 -0500

    fix: followed luck and epicenter detection pings dont work properly

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -30,7 +30,7 @@ module.exports = {
     // check if ?luckping or luckping is sent in the luck channel
     const serverPartnerText = `\n-# this host is part of our <#1393424277180776619>`;
     if (isFromFollowedServer) {
-
+      console.log("Received message from followed server: ", message.content);
       const FOLLOWED_LUCK_CHANNEL_ID = "1393401001029140610"; // replace with your actual channel ID
       const LUCK_PING_ROLE_ID = "1393774571336896592"; // replace with your actual role ID
       if (channel.id === FOLLOWED_LUCK_CHANNEL_ID) {
@@ -39,7 +39,7 @@ module.exports = {
         if (messageContent.includes("?luckparty") || messageContent.includes("luckping") || messageContent.includes("@Luck")) {
           console.log("Luck ping message received: ", messageContent);
           await channel.send(
-            `<@${message.author.id}> is hosting a luck party!${serverPartnerText}`
+            `<@${message.author.id}> is hosting a luck party! <@${LUCK_PING_ROLE_ID}>${serverPartnerText}`
           );
         }
       }
@@ -48,11 +48,10 @@ module.exports = {
       const EPICENTER_PING_ROLE_ID = "1436523924472070187"; // replace with your actual role ID
       if (channel.id === FOLLOWED_EPICENTER_CHANNEL_ID) {
         const messageContent = message.content.toLowerCase();
-        // make sure the message is from a followed server
         if (messageContent.includes("epiping")) {
           console.log("Epicenter ping message received: ", messageContent);
           await channel.send(
-            `<@${message.author.id}> is hosting a trip to the epicenter! (<@&${EPICENTER_PING_ROLE_ID}${serverPartnerText}>) `
+            `<@${message.author.id}> is hosting a trip to the epicenter! <@&${EPICENTER_PING_ROLE_ID}>${serverPartnerText} `
           );
         }
       }
