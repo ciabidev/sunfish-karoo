01a645cf9dcb1b9e6c656daa30433a213012b418
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-01-22 19:06:37 -0500

    change the moderation dm message from embed to componentsv2

--- src/modules/sendModerationDM.js
+++ src/modules/sendModerationDM.js
@@ -1,5 +1,7 @@
 
 
+const { ContainerBuilder, MessageFlags } = require("discord.js");
+
 module.exports = async function sendModerationDM({
   targetUser,
   action,
@@ -10,31 +12,38 @@ module.exports = async function sendModerationDM({
   pointsDelta = null,
 }) {
   let description = `
-  **Server:** ${interaction.guild.name} \n
-  **Action:** ${action} \n
-  **Actioned By:** <@${actionedBy.id}> \n
-  **Reason:** ${reason ? reason : "No reason provided"} \n
-  `;
+**Server:** ${interaction.guild.name}
+**Action:** ${action}
+**Actioned By:** <@${actionedBy.id}>
+**Reason:** ${reason ? reason : "No reason provided"}
+`;
 
-  if (durationMs) {
-    description += `**Duration:** ${interaction.client.modules.formatMilliseconds(durationMs)}\n`;
-  }
+if (durationMs) {
+  description += `**Duration:** ${interaction.client.modules.formatMilliseconds(durationMs)}\n`;
+}
 
-  if (pointsDelta) {
-    description += `**Point Change:** ${pointsDelta > 0 ? "+" : ""}${pointsDelta}\n`;
-  }
+if (pointsDelta) {
+  description += `**Point Change:** ${pointsDelta > 0 ? "+" : ""}${pointsDelta}\n`;
+}
 
-  try {
-    await targetUser.send({
-      embeds: [
-        {
-          title: `You (probably) broke a rule in ${interaction.guild.name}`,
-          description: `a moderation action was applied... \n ${description}`,
-          color: 0xff0000,
-        },
-      ],
-    });
-  } catch (err) {
-    console.error(`Failed to DM ${targetUser.id}:`, err);
+const mainText = new ContainerBuilder()
+  .addTextDisplayComponents(
+    (t) => t.setContent(`### You (probably) broke a rule in ${interaction.guild.name}`))
+  .addTextDisplayComponents(
+    (t) => t.setContent(`A moderation action was applied... ${description}`)
+  )
+
+
+  const components = [mainText];
+
+  if (interaction) {
+    try {
+      await targetUser.send({
+        components,
+        flags: MessageFlags.IsComponentsV2,
+      });
+    } catch (err) {
+      console.error(`Failed to DM ${targetUser.id}:`, err);
+    }
   }
 };
