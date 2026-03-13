1998145c8c5a25782ec2c86e2565691f692755ec
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-03-12 21:55:45 -0400

    add verify channel to welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,7 +9,7 @@ module.exports = {
     const channel = member.guild.channels.cache.get(CHANNEL_ID);
     if (!channel) return;
     await channel.send(
-      `<@&${WELCOME_ROLE_ID}> ➡️ Welcome ${member} to Sunfish Village! Go to <#1478565020194443265> to find or create Raids and Parties \n-# we now have ${member.guild.memberCount} members`,
+      `<@&${WELCOME_ROLE_ID}> ➡️ Welcome ${member} to Sunfish Village! Go to <#1478565020194443265> to find or create Raids and Parties. Please verify to gain access to the rest of the server <@&${VERIFY_CHANNEL_ID}> \n-# we now have ${member.guild.memberCount} members`,
     );
   },
 };
