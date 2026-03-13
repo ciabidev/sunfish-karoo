65781f2d20ec5c48d3b73477783a5245ca71c8f4
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-03-13 17:42:18 -0400

    update ping command to also ping supabase

--- commands/system/ping.js
+++ commands/system/ping.js
@@ -1,8 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('check if Karoo is ok'),
+	data: new SlashCommandBuilder().setName('ping').setDescription('check if Karoo is ok, also ping supabase'),
 	async execute(interaction) {
 		await interaction.reply('hi');
+		await interaction.client.modules.supabase.getCases(0);
 	},
 };
