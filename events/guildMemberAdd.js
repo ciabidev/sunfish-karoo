20a83bf96a5fad6480754d7bf691fa3aa997dd3a
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-03-12 21:42:34 -0400

    update welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,8 +4,12 @@ module.exports = {
   name: Events.GuildMemberAdd,
   async execute(member) {
     const CHANNEL_ID = "1393752798625140757";
+    const WELCOME_ROLE_ID = "1481829069422067712  ";
+    const VERIFY_CHANNEL_ID = "1393752798625140757";
     const channel = member.guild.channels.cache.get(CHANNEL_ID);
     if (!channel) return;
-    await channel.send(`➡️ Welcome ${member} to Sunfish Village! \n-# we now have ${member.guild.memberCount} members`);
+    await channel.send(
+      `➡️ <@&${WELCOME_ROLE_ID}> Welcome ${member} to Sunfish Village! Please verify in <#1393748197117005958> to see all channels. \n-# we now have ${member.guild.memberCount} members`,
+    );
   },
 };
