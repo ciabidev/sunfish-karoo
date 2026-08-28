0a52d35eae35d8c6e025e0d7a8a5e616bc6c0248
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-08-28 12:01:31 -0400

    update welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,6 @@ module.exports = {
     if (!channel) return;
     await channel.send(
       `<@&1481829069422067712> ➡️ Welcome ${member} to Sunfish Village! <#1393748197117005958> 
-- **Looking for quick help/services?** Ping in <#1463002217886908496>. We have tons of active helpers ready for the job
 -# In our Lantern Festival this winter, you can get **free items for hosting parties** https://discord.com/channels/1393397825085374587/1542609776066699354/1542613981502373929`,
     );
   },
